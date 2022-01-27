ef793eee1b2c6b10b21a32779c3b34eda859a995
adubois31 <duboisaubry1110@gmail.com>
2022-01-27 19:58:31 +0100

    more comments

--- floppeX/src/main/java/FloppaChat/Network/BroadcastClient.java
+++ floppeX/src/main/java/FloppaChat/Network/BroadcastClient.java
@@ -20,31 +20,31 @@ public class BroadcastClient {
 
         byte[] buffer = broadcastMessage.getBytes();
 
-        DatagramPacket packet 
-          = new DatagramPacket(buffer, buffer.length, address, PortNb);
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, PortNb);
+        //sends the  broadcastMessage to all users
         socket.send(packet);
         
         byte[] buff_answer = new byte[512];
+        //setting a timeout to give time to all users to answer the broadcast message
         socket.setSoTimeout(timeout);
             
 		 while(true){
 	            try {
 	            	DatagramPacket packet1 = new DatagramPacket(buff_answer,buff_answer.length);
 	                socket.receive(packet1);
+	                //creating a thread for the user who answered
 	                Thread BroadcastMultAnsThread = new Thread(new Runnable() {
 	                	@Override
 	                	public void run() {
-	                		System.out.println("Nouveau thread réponse");
 	                		Process process = new Process();
 	                		try {
-	                			System.out.println(packet1.getAddress().toString());
+	                			//processes this packet
 	                			process.BroadcastProcess(packet1, socket);
 	                		} catch (IOException e) {
 	                			e.printStackTrace();
 	                		}
 	                	}
 	                });
-	                System.out.println("Starting thread ");
 	                BroadcastMultAnsThread.start();
 	            }
 	            catch (SocketTimeoutException e) {
@@ -54,7 +54,6 @@ public class BroadcastClient {
 	                break;
 	            }
 	        }
-		 System.out.println("Out of the client while");
         
     }
 }
--- floppeX/src/main/java/FloppaChat/Network/MultiClientConnections.java
+++ floppeX/src/main/java/FloppaChat/Network/MultiClientConnections.java
@@ -9,28 +9,35 @@ import FloppaChat.DataBase.ActiveUserManager;
 import FloppaChat.DataBase.DBController;
 import FloppaChat.GUI.Global;
 
+//manages all the connections as a client when chatting with the users
 public class MultiClientConnections {
 	
 	public static ArrayList<MessageClient> ClientConnections = new ArrayList<>();
 	
+	
 	public static void SendMessAsClient(String TargetIP, String Message) {
 		boolean alreadyConnected = false;
 		for (MessageClient MC : ClientConnections) {
+			//if the (server) user connection is already made and stored in ClientConnection sends it to him
 			if (MC.getRemoteIP().equals(TargetIP)){
 				MC.SendMessToServer(Message);
 				alreadyConnected=true;
 				break;
 			}
 		}
+		//if it's the 1st time we want to send a message to this server
 		if(!(alreadyConnected)) {
 			Socket socket = null;
 			ActiveUserManager aUM = new ActiveUserManager();
 			try {
+				//we create a client socket and MessageClient
 				socket = new Socket(TargetIP,Global.MessServNb);
 				MessageClient MC = new MessageClient(socket);
+				//we add him to our client connections and to the db if not already done
 				ClientConnections.add(MC);
 				DBController DBC = new DBController(Global.dbName);
 				DBC.createUser(aUM.getActiveUserPseudo(TargetIP),TargetIP);
+				//starts the message client thread to wait for the server messages and sends the message to the server
 				MC.RecvMessFromServer();
 				MC.SendMessToServer(Message);
 			} catch (IOException e) {
@@ -43,19 +50,19 @@ public class MultiClientConnections {
 			System.out.println("Client Connection is alive : "+MC);
 		}
 	}
+	
+	//closing all the connections with the servers (as a client)
 	public static void ClosingClients() {
 		PrintingClientConnections();
 		Iterator<MessageClient> itr = ClientConnections.iterator();
 		while(itr.hasNext()) {
 			MessageClient MC = itr.next();
-			System.out.println("Interrupting client : "+MC);
 			MC.EndChat();
 			removeClient(itr);
 		}
-		System.out.println("Clients who survived :");
-		PrintingClientConnections();
 	}
 	
+	//removes safely a connection to a server from our ClientConnections iterator
 	public static synchronized void removeClient(Iterator<MessageClient> itr) {
 		itr.remove();
 	}
