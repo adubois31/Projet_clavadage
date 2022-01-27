ad346ed0a18ddb8700a6eb6ce5f2bf967a842c11
Totor <git.demo@gmail.com>
2022-01-27 17:52:18 +0100

    Some changes

--- floppeX/src/main/java/FloppaChat/floppeX/App.java
+++ floppeX/src/main/java/FloppaChat/floppeX/App.java
@@ -1,7 +1,6 @@
 package FloppaChat.floppeX;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
